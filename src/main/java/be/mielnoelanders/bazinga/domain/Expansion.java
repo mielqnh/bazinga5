c5bcc1e6f7e5fb39eab559d889bbd324464bf3df
Miel <miel.noelanders@qnh.Be>
2018-03-06 15:01:39 +0100

    branchblatest

--- src/main/java/be/mielnoelanders/bazinga/domain/Expansion.java
+++ src/main/java/be/mielnoelanders/bazinga/domain/Expansion.java
@@ -16,7 +16,6 @@ public class Expansion extends AbstractEntity implements Serializable {
     private Publisher publisher;
     private int stock;
     private Supplier supplier;
-    private Price price;
 
     // FIELDS WITH MAPPINGS
     @Column(name = "EXPANDS")
