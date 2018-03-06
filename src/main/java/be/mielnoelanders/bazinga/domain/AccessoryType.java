b338010d5c03dbc2b52f3ada68facb4e203e4999
Miel <miel.noelanders@qnh.Be>
2018-03-06 16:17:22 +0100

    commentaar in accessorytype aangepast commit en push

--- src/main/java/be/mielnoelanders/bazinga/domain/AccessoryType.java
+++ src/main/java/be/mielnoelanders/bazinga/domain/AccessoryType.java
@@ -1,5 +1,7 @@
 package be.mielnoelanders.bazinga.domain;
 
+//laatste local commit voor de push naar github
+
 public enum AccessoryType {
     SLEEVES, DICE, PLAYMATS, DECKBOXES, BINDERS, BINDERPAGES, OTHERS
 }
