32607da13411921e98e9713bacf15fed5650e840
Miel <miel.noelanders@qnh.Be>
2018-03-07 11:55:53 +0100

    h2proberen

--- src/test/java/be/mielnoelanders/bazinga/service/GameServiceTest.java
+++ src/test/java/be/mielnoelanders/bazinga/service/GameServiceTest.java
@@ -3,11 +3,9 @@ package be.mielnoelanders.bazinga.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@ActiveProfiles("test")
 @SpringBootTest
 public class GameServiceTest{
 
