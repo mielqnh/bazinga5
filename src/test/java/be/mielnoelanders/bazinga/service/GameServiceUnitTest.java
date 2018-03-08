7fe64dd30cafcb57a6542939046dd1a7d8e0b50d
Miel <miel.noelanders@qnh.Be>
2018-03-08 15:40:10 +0100

    GetOneTest Werkt

--- src/test/java/be/mielnoelanders/bazinga/service/GameServiceUnitTest.java
+++ src/test/java/be/mielnoelanders/bazinga/service/GameServiceUnitTest.java
@@ -23,32 +23,44 @@ public class GameServiceUnitTest {
     @Mock
     private GameRepository repository;
 
-    private Game testGame;
-    private Optional<Game> testOptionalGame;
+    private Game testGame1;
+    private Optional<Game> testOptionalGame1;
+
+    private Game testGame2;
+    private Optional<Game> testOptionalGame2;
+
+    private Game testGame3;
+    private Optional<Game> testOptionalGame3;
 
     @Before
     public void init(){
-        Game.Builder game = new Game.Builder();
-        game.title("testgame").edition(1);
-        testGame = game.build();
-        testOptionalGame = Optional.of(testGame);
+        Game.Builder game1 = new Game.Builder();
+        game1.title("testgame1").edition(1);
+        testGame1 = game1.build();
+        testOptionalGame1 = Optional.of(testGame1);
+
+        Game.Builder game2 = new Game.Builder();
+        game2.title("testgame2").edition(2);
+        testGame2 = game2.build();
+        testOptionalGame2 = Optional.of(testGame2);
+
+        Game.Builder game3 = new Game.Builder();
+        game3.title("testgame3").edition(3);
+        testGame3 = game3.build();
+        testOptionalGame3 = Optional.of(testGame3);
     }
 
     @Test
     public void getOneTest(){
 
         // Hier zeg je wat de mock moet geven als je de findById oproept op de repository.
-        Mockito.when(this.repository.findById(3L)).thenReturn(testOptionalGame);
+        Mockito.when(this.repository.findById(3L)).thenReturn(testOptionalGame1);
 
         // Hier roep je de findById op de repository aan VIA de service getOne
         Game resultFromService = this.gameService.getOne(3L);
 
-//        System.out.println(testOptionalGame);
-//        System.out.println(resultFromService);
-//        System.out.println(resultFromService.getTitle());
+        assertEquals("testgame1", resultFromService.getTitle());
 
-        assertEquals("testgame", resultFromService.getTitle());
-
-        //Mockito.verify(this.repository).getOne(3L);
+        Mockito.verify(this.repository, Mockito.times(1)).findById(3L);
     }
 }
