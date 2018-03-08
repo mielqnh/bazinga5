8b5f027ff92fce9dab70721c79bce4c66f5533fc
MarcoQnH <marco.kohnen@qnh.be>
2018-03-08 13:01:31 +0100

    RequestMapping for findOne

--- src/main/java/be/mielnoelanders/bazinga/restcontroller/CustomerEndPoint.java
+++ src/main/java/be/mielnoelanders/bazinga/restcontroller/CustomerEndPoint.java
@@ -5,10 +5,7 @@ import be.mielnoelanders.bazinga.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/customer")
@@ -33,6 +30,16 @@ public class CustomerEndPoint {
             return new ResponseEntity<Iterable<Customer>>(HttpStatus.NO_CONTENT);
         } else {
             return new ResponseEntity<Iterable<Customer>>(customers, HttpStatus.OK);
+        }
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public ResponseEntity<Customer> findOne(@PathVariable Long id) {
+        Customer customerFound = customerService.findOne(id);
+        if (customerFound == null) {
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<Customer>(customerFound, HttpStatus.OK);
         }
     }
 }
