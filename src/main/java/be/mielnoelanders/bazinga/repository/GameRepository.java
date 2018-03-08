85c0702739bea6c6777eabeac6b439f6e789db2f
Miel <miel.noelanders@qnh.Be>
2018-03-08 11:03:25 +0100

    Gamerepository klaar

--- src/main/java/be/mielnoelanders/bazinga/repository/GameRepository.java
+++ src/main/java/be/mielnoelanders/bazinga/repository/GameRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GameRepository extends JpaRepository<Game,Long> {
 
+    Iterable<Game> findByTitle(String name);
+
 }
--- src/main/java/be/mielnoelanders/bazinga/service/GameService.java
+++ src/main/java/be/mielnoelanders/bazinga/service/GameService.java
@@ -6,5 +6,18 @@ public interface GameService {
 
     Iterable<Game> getAll();
 
+    //getById()
+    Game getOne(Long id);
+
+    //delete()
+    void deleteById(Long id);
+
+    //create()
+    Game insertGame(Game game);
+
+    //update()
+    Game changeGame(Long id, Game game);
+
+    Iterable<Game> findByName(String name);
 
 }
