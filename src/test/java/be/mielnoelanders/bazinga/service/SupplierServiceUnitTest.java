9852704f0c8ae664cc9a9e57edfb92f6c0e469f7
MarcoQnH <marco.kohnen@qnh.be>
2018-03-09 15:50:08 +0100

    SupplierServiceUnitTest : method testFindAll

--- src/test/java/be/mielnoelanders/bazinga/service/SupplierServiceUnitTest.java
+++ src/test/java/be/mielnoelanders/bazinga/service/SupplierServiceUnitTest.java
@@ -10,6 +10,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -20,13 +23,14 @@ public class SupplierServiceUnitTest {
     @InjectMocks
     private SupplierServiceImpl supplierService;
 
-    @Mock
+    @Mock //is injected into supplierService
     private SupplierRepository supplierRepository;
 
     private Supplier supplier1;
     private Supplier supplier2;
     private Supplier supplier3;
     private Optional<Supplier> optionalSupplier;
+    private List<Supplier> suppliers;
 
     @Before
     public void init() {
@@ -37,8 +41,9 @@ public class SupplierServiceUnitTest {
         supplier3 = new Supplier();
         supplier3.setName("testsupplier3");
         optionalSupplier = Optional.of(supplier2);
+        suppliers = new ArrayList<>();
+        suppliers.addAll(Arrays.asList(supplier1, supplier2, supplier3));
     }
-
 
     @Test
     public void testFindById() {
@@ -46,5 +51,15 @@ public class SupplierServiceUnitTest {
         Mockito.when(this.supplierRepository.findById(2L)).thenReturn(optionalSupplier);
         Supplier supplierFromService = supplierService.findById(2L);
         assertThat(supplierFromService.getName()).isEqualTo("testsupplier2");
+    }
+
+    @Test
+    public void testFindAll() {
+        Mockito.when(this.supplierRepository.findAll()).thenReturn(suppliers);
+        Iterable<Supplier> suppliersFromService = supplierService.findAll();
+        //test op name van eerste supplier in de List suppliers
+        assertThat(((List<Supplier>) suppliersFromService).get(0).getName()).isEqualTo("testsupplier1");
+        //test of aantal suppliers in de List suppliers 3 is
+        assertThat((List<Supplier>) suppliersFromService).size().isEqualTo(3);
     }
 }
