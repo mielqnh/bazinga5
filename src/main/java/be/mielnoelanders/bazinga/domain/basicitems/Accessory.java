52d0b92fdaeb4b21aa429d991ef2e7e94902e252
Miel <miel.noelanders@qnh.Be>
2018-03-16 13:41:31 +0100

    rqyu

--- src/main/java/be/mielnoelanders/bazinga/domain/basicitems/Accessory.java
+++ src/main/java/be/mielnoelanders/bazinga/domain/basicitems/Accessory.java
@@ -1,12 +1,8 @@
 package be.mielnoelanders.bazinga.domain.basicitems;
 
 import be.mielnoelanders.bazinga.domain.enums.AccessoryType;
-import be.mielnoelanders.bazinga.domain.other.Publisher;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 
 @Entity
 public class Accessory extends Item {
@@ -15,11 +11,6 @@ public class Accessory extends Item {
     private String name;
     private AccessoryType type;
 
-    // FIELDS WITH MAPPINGS
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "publisher_id")
-    private Publisher publisher;
-
     // CONSTRUCTORS
     public Accessory() {
     }
@@ -27,7 +18,6 @@ public class Accessory extends Item {
     private Accessory(Accessory.Builder builder) {
         name = builder.name;
         type = builder.type;
-        publisher = builder.publisher;
     }
 
     // GETTERS & SETTERS
@@ -44,12 +34,6 @@ public class Accessory extends Item {
     public void setType(AccessoryType type) {
         this.type = type;
     }
-    public Publisher getPublisher() {
-        return publisher;
-    }
-    public void setPublisher(Publisher publisher) {
-        this.publisher = publisher;
-    }
 
     // OVERRIDES
     @Override
@@ -57,34 +41,24 @@ public class Accessory extends Item {
         return "Accessory{" +
                 "name='" + name + '\'' +
                 ", type=" + type +
-                ", publisher=" + publisher +
                 '}';
     }
 
     // BUILDER
     public static final class Builder {
         private String name;
-        private Publisher publisher;
         private AccessoryType type;
 
         public Builder() {
         }
-
         public Accessory.Builder name(String val) {
             name = val;
             return this;
         }
-
         public Accessory.Builder type(AccessoryType val) {
             type = val;
             return this;
         }
-
-        public Accessory.Builder publisher(Publisher val) {
-            publisher = val;
-            return this;
-        }
-
         public Accessory build() {
             return new Accessory(this);
         }
